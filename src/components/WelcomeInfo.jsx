56922b5a8eba1af8d5643816ba165286cd70eda5
Yaups <tpallison.ta@gmail.com>
2024-08-08 13:45:27 +0100

    Changed welcome text

--- src/components/WelcomeInfo.jsx
+++ src/components/WelcomeInfo.jsx
@@ -4,10 +4,11 @@ const WelcomeInfo = () => (
     <a href="https://fullstackopen.com/" target="_blank" rel="noreferrer">
       Fullstackopen{' '}
     </a>
-    course, this is a single-page application written in React. Feel free to log
-    in using the supplied username and password and try out the site, or click
-    the &quot;About&quot; tab for more info about me, this website, and how it
-    was developed!
+    course, this is a single-page application written in React. Feel free to
+    have a look around and try out the site, or click the &quot;About&quot; tab
+    for more info about me, this website, and how it was developed! I have
+    decided to disable user account creation to avoid spam posts, but you can
+    still view all posted blogs without being logged in.
   </p>
 )
 
